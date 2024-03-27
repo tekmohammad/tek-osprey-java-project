d25e4aac28a4b04c1837ae8aa39ad8297257f1d7
Mohammad Shokriyan <mohammad@tekschool.us>
2024-03-26 21:23:57 -0400

    adding first class

---
+++ src/tek/git/CalculateSomething.java
@@ -0,0 +1,10 @@
+package tek.git;
+
+public class CalculateSomething {
+
+    public static void main(String[] args) {
+        int result = 10 + 10;
+
+        System.out.println("Result is " + result);
+    }
+}
