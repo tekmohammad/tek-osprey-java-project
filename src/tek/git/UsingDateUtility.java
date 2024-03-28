c916124c625b4a443df9b5203a186271a677c4f8
Mohammad Shokriyan <mohammad@tekschool.us>
2024-03-27 21:00:39 -0400

    Update date utility

--- src/tek/git/UsingDateUtility.java
+++ src/tek/git/UsingDateUtility.java
@@ -2,11 +2,14 @@ package tek.git;
 
 import tek.git.utility.DateUtility;
 
+import java.time.LocalDateTime;
+
 public class UsingDateUtility {
 
     public static void main(String[] args) {
         DateUtility utility = new DateUtility();
-
-        System.out.println(utility.now());
+        LocalDateTime now =utility.now();
+        System.out.println("Day of the Month is "
+                + now.getDayOfMonth());
     }
 }
